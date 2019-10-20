bb0a0095c714a725778e778da849e1ab2b3ed20d
Kenny Chhoeun <kchhoeun20703@gmail.com>
2019-10-20 16:43:40 -0700

    Update AES128.cpp

--- AES128.cpp
+++ AES128.cpp
@@ -6,21 +6,34 @@
 #include <string>
 #include <termios.h>
 #include <unistd.h>
-#include <vector>
 using namespace std;
 
-void HexOutput(unsigned char c){
-    if(c/16 < 10)
-        cout << (char)((c/16) + '0');
-    if(c/16 >= 10)
-        cout << (char)(((c/16)-10)+'A');
-    if(c%16 < 10)
-        cout << (char)((c%16)+'0');
-    if(c%16 >= 10)
-        cout << (char)(((c%16)-10)+'A');
+// I believe these methods of hiding keystrokes
+// are operating system specific
+// so they more than likely will not work on a windows
+//based machine, only _nux machines
+void hide_Keystrokes()
+{
+    //part of termois.h
+    termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    //disabling echo and keystroke
+    tty.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
 }
 
-void encExtension(string& s, const string& newExt){
+void show_Keystrokes()
+{
+    //part of termois.h
+    termios tty;
+    tcgetattr(STDIN_FILENO, &tty);
+    //enable echo back so terminal works normally
+    tty.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &tty);
+}
+
+//function to create the file name with new extension
+void fileExtension(string& s, const string& newExt){
     string::size_type i = s.rfind('.',s.length());
     if(i!=string::npos) {
         s.replace(i+1, newExt.length(), newExt);
@@ -28,14 +41,14 @@ void encExtension(string& s, const string& newExt){
 }
 
 void KeyExpansionCore(unsigned char* in, unsigned char i) {
-	//rotate left 1 byte
+	//rotate left
 	unsigned char t = in[0];
 	in[0] = in[1];
 	in[1] = in[2];
 	in[2] = in[3];
 	in[3] = t;
 
-	//sbox 
+	//sbox in AES128.h//
 	in[0] = sbox[in[0]];
 	in[1] = sbox[in[1]];
 	in[2] = sbox[in[2]];
@@ -59,7 +72,7 @@ void KeyExp(unsigned char* inputKey, unsigned char* expKey){
 	int generated_Bytes = 16; //16 from our for loop
 	int iterations = 1;
 	unsigned char tmp[4];
-
+    //expanded key
 	while (generated_Bytes < 176) {
 		for (int i = 0; i < 4; i++) {
 			tmp[i] = expKey[i + generated_Bytes - 4];
@@ -77,10 +90,9 @@ void KeyExp(unsigned char* inputKey, unsigned char* expKey){
 		}
 	}
 }
-
+//shift rows function
 void shift_rows(unsigned char* state) {
 	unsigned char tmp[16];
-	//shifting
 	tmp[0] = state[0];
 	tmp[1] = state[5];
 	tmp[2] = state[10];
@@ -101,7 +113,7 @@ void shift_rows(unsigned char* state) {
 	tmp[14] = state[6];
 	tmp[15] = state[11];
 
-	//copy into state
+	//copy all the temporary values from tmp array to our state array
 	for (int i = 0; i < 16; i++)
 	{
 		state[i] = tmp[i];
@@ -109,8 +121,11 @@ void shift_rows(unsigned char* state) {
 }
 
 void mix_columns(unsigned char* state) {
-	//research for galois field 
+	//research for galois field
+    //use the lookup table mul2 and mul3 from AES128.cpp
+    //every round except for the last one
 	unsigned char tmp[16];
+    //all casting to unsigned chars and xor'ed
 	tmp[0] = (unsigned char)(mul2[state[0]] ^ mul3[state[1]] ^ state[2] ^ state[3]);
 	tmp[1] = (unsigned char)(state[0] ^ mul2[state[1]] ^ mul3[state[2]] ^ state[3]);
 	tmp[2] = (unsigned char)(state[0] ^ state[1] ^ mul2[state[2]] ^ mul3[state[3]]);
@@ -130,26 +145,27 @@ void mix_columns(unsigned char* state) {
 	tmp[13] = (unsigned char)(state[12] ^ mul2[state[13]] ^ mul3[state[14]] ^ state[15]);
 	tmp[14] = (unsigned char)(state[12] ^ state[13] ^ mul2[state[14]] ^ mul3[state[15]]);
 	tmp[15] = (unsigned char)(mul3[state[12]] ^ state[13] ^ state[14] ^ mul2[state[15]]);
-	//copy over to state
+	//copy over the temporary values to the state array
 	for (int i = 0; i < 16; i++)
 	{
 		state[i] = tmp[i];
 	}
 }
-
+//key addition layer
 void keyAdd(unsigned char* state, unsigned char* roundKey) {
 	for (int i = 0; i < 16; i++) {
 		        //bitwise xor
 		state[i] ^= roundKey[i];
 	}
 }
-
+//sub bytes layer
 void sub_bytes(unsigned char* state) {
 	for (int i = 0; i < 16; i++) {
 		state[i] = sbox[state[i]];
 	}
 }
-//was unsigned char* key) for line 148
+
+//encryption process
 void AES128Encrypt(unsigned char* m, unsigned char* key) {
 	unsigned char state[16]; 
 	for (int i = 0; i < 16; i++) {
@@ -168,28 +184,15 @@ void AES128Encrypt(unsigned char* m, unsigned char* key) {
 		keyAdd(state, expKey + (16 *(i+1)));
 	}
 
-	//last round, no mix columns here
+	//last round, no mix is done columns here
 	sub_bytes(state);
 	shift_rows(state);
 	keyAdd(state, expKey + 160);
-
+    
 	for (int i = 0; i < 16; i ++) {
 		m[i] = state[i];
 	}
 
-}
-
-
-char* readFileBytes(const char *name)
-{
-    ifstream fl(name);
-    fl.seekg( 0, ios::end );
-    size_t len = fl.tellg();
-    char *ret = new char[len];
-    fl.seekg(0, ios::beg);
-    fl.read(ret, len);
-    fl.close();
-    return ret;
 }
 
 
@@ -199,39 +202,53 @@ int main(int argc, char* argv[])
     ofstream dst_filepath;
     
     string inputFileName;
-	unsigned char m[] = "qwertyuiopasdfgh";
     
     if (argc < 2) {
         cout << "Not Enough Arguments given, format is " << endl;
         cout << "./AES128Encrypt <file_to_be_encrypted.txt>" << endl;
-        exit(1);
+        exit(1); //if the user started the program wrong they have to restart it
     }
     inputFileName = argv[1];
-    
-    //read the input file binary here ***
-    ifstream in(inputFileName, ios::binary);
-    
-    cout << "Please input the encryption key in hex values seperated by spaces\n";
-    cout << "eg. 01 02 03 04 05 06 07 07 09 0a 0b 0c 0d 0e 0f 10: \n";
+    //read the input file by binary//
+    size_t size;
+    src_filepath.open(inputFileName, ios::in | ios::binary|ios::ate);
+    char* m = 0;
+    src_filepath.seekg(0, ios::end); //pointer ios::ate: at the end
+    size = src_filepath.tellg(); //length of the file
+    src_filepath.seekg(0, ios::beg);//pointer at beginning
+    m = new char[size+1]; //char array pertain to size of input file
+    src_filepath.read(m,size);
+    m[size] = '\0';
+
+    //print out statements with instructions for the user//
+    cout << "\n\n";
+    cout << "-----------------128 bit AES ENCRYPTION PROGRAM------------------\n";
+    cout << "Please input the 128-bit key in hex values seperated by spaces\n";
+    cout << "     e.g 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f 10\n: ";
 
     string keyInput;
+    hide_Keystrokes();
     getline(cin,keyInput);
-    
+    show_Keystrokes();
+    //convert the input into chars for the key
     istringstream hex_chars_stream(keyInput);
     unsigned char key[16];
     int i=0;
     unsigned int c;
+    
     while(hex_chars_stream >> hex >> c)
     {
-        key[i] = c;
+        key[i] = c; //add to the key array
         i++;
     }
     
-    encExtension(inputFileName, "enc");
-    //dst_filepath.open(inputFileName);
-    //will create a output file with same name
-    //with but with .enc extension
+    fileExtension(inputFileName, "enc");
+    //call function that will create a output
+    //file with same name
+    //with but with different extension we pass as the second argument
+    //updates inputFileName value
     
+    //padding
 	int msglength = strlen((const char*)m);
 	int paddedmsgLength = msglength;
 
@@ -240,6 +257,7 @@ int main(int argc, char* argv[])
 	}
 
 	unsigned char* paddedMsg = new unsigned char[paddedmsgLength];
+    
 	for (int i = 0; i < paddedmsgLength; i++) {
 		if (i >= msglength)
 			paddedMsg[i] = 0;
@@ -252,28 +270,25 @@ int main(int argc, char* argv[])
 		AES128Encrypt(paddedMsg+i, key);
 	}
 
-	for (int i = 0; i < paddedmsgLength; i++) {
-        HexOutput(paddedMsg[i]);
-        cout << " ";
-	}
-    
     dst_filepath.open(inputFileName, ios::out | ios::binary);
+    //prints the result to the file with extension .enc in same directory
     if (dst_filepath.is_open())
     {
         for(int i=0;i<paddedmsgLength;i++){
             dst_filepath << paddedMsg[i];
         }
         dst_filepath.close();
-        cout << "Wrote encrypted message to: " << inputFileName <<  endl;
+        cout << "Wrote encrypted file to: " << inputFileName <<  endl;
+        //tell the user the encryption done to new file
     }
     else
         cout << "Unable to open file";
+        //if something where to go wrong
     
-    // Free memory //
+    //Free up some memory
     delete[] paddedMsg;
+    delete[] m;
     
-    return 0;
-    
-    cout << "\nEncryption Complete √" << endl;
+    cout << "\nEncryption Complete √\n\n" << endl;
 	return 0;
 }
